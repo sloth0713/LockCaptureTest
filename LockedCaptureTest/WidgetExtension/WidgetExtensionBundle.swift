54a638f2739b2f3e2ec1a2e3bf6b06c81780ce66
严加薪 <yanjiaxin.0713@bytedance.com>
2025-02-21 11:55:29 +0800

    feat:add ab

--- LockedCaptureTest/WidgetExtension/WidgetExtensionBundle.swift
+++ LockedCaptureTest/WidgetExtension/WidgetExtensionBundle.swift
@@ -9,6 +9,43 @@ import WidgetKit
 import SwiftUI
 
 @main
+struct WidgetLauncher {
+    static func main() {
+        
+        if widgetEnable {
+            WidgetExtensionBundle.main()
+        }else {
+            EmptyWidget.main()
+        }
+
+    }
+    
+    static var widgetEnable: Bool {
+        
+        let random = Double(Int(arc4random_uniform(1000))) / 1000.0
+        print("random \(random)")
+        
+        var enableWidget:Bool = random > 0.5
+        enableWidget = true
+        if (enableWidget){
+            print ("enableWidget")
+            return true
+        }else {
+            print ("disableWidget")
+            return false
+        }
+        
+    }
+}
+
+
+struct EmptyWidget: WidgetBundle {
+    @WidgetBundleBuilder
+    var body: some Widget {
+        WidgetBundleBuilder.buildBlock()
+    }
+}
+
 struct WidgetExtensionBundle: WidgetBundle {
     var body: some Widget {
 //        WidgetExtension()
